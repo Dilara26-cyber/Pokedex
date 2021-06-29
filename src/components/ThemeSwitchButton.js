981b22ee1b81995c3adb9cd54d596aab7ac07a3b
Dilara Aksoy <70946845+Dilara26-cyber@users.noreply.github.com>
2021-06-29 16:30:31 +0300

    Remove Local Storage Bug

--- src/components/ThemeSwitchButton.js
+++ src/components/ThemeSwitchButton.js
@@ -1,26 +1,21 @@
 import { useState, useEffect } from "react"
 import {FiSun} from "react-icons/fi"
 import {BsMoon} from "react-icons/bs"
-const theme = JSON.parse(localStorage.getItem("theme") || "light")
-const checked = JSON.parse(localStorage.getItem("checked") || true)
+
 const ThemeSwitchButton = () => {
-    const [isChecked, setIsChecked] = useState(checked)
-    const [themeValue, setThemeValue] = useState(theme)
+    const [isChecked, setIsChecked] = useState(true)
+    const [themeValue, setThemeValue] = useState("light")
     const handleChange = () => {
         setIsChecked(prev => !prev)
         if(isChecked === true) {
             setThemeValue("dark")
-            document.documentElement.setAttribute("data-theme", themeValue)
         }else{
             setThemeValue("light")
-            document.documentElement.setAttribute("data-theme", themeValue)
         }
     }
     useEffect(() => {
-        localStorage.setItem("theme", JSON.stringify(themeValue))
-        localStorage.setItem("checked", JSON.stringify(isChecked))
         document.documentElement.setAttribute("data-theme", themeValue)
-      }, [isChecked, themeValue])
+      }, [themeValue])
     return ( 
         <div className="theme-switch-container">
             <span id="toggle-icon">
